439fe560d8455a26556ff0140f32de7e57fd0d24
JOliver23 <59593045+JOliver23@users.noreply.github.com>
2020-08-11 19:22:56 -0700

    prj init; Form skeleton built; imported to App; Temp component for list saved in state

---
+++ app-name/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Form from './components/Form';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>app-name Sign-Ups</h1>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
---
+++ app-name/src/components/Form.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import * as Yup from "yup";
+
+function Form() {
+    return (
+        <form>
+            <label htmlFor="name">
+                Name:
+                <input id="name" type="name" name="name" placeholder="name" />
+            </label>
+            <label htmlFor="email">
+                Name:
+                <input id="email" type="email" name="email" placeholder="email" />
+            </label>
+            <label htmlFor="password">
+                Password
+                <input id="password" type="password" name="password" placeholder="Password" />
+            </label>
+            <label htmlFor="terms">
+                Do you agree to the terms and conditions?
+                <input id="terms" type="radio" name="terms" />
+            </label>
+            <button>Submit!</button>
+        </form>
+    )
+};
+
+export default Form;
