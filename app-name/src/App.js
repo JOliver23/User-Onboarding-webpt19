4c4e822b14eb75785acf1dd4d1622efa4f86f375
JOliver23 <59593045+JOliver23@users.noreply.github.com>
2020-08-13 16:03:15 -0700

    MVP

--- app-name/src/App.js
+++ app-name/src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import Form from './components/Form';
-import SignUpList from './components/SignUpList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>app-name Sign-Ups</h1>
+      <h1>Squad Sign-Ups</h1>
       <Form />
-      <SignUpList />
     </div>
   );
 }
--- app-name/src/components/Form.js
+++ app-name/src/components/Form.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect }  from 'react';
-import * as Yup from "yup";
+import * as yup from "yup";
+import axios from "axios";
 
 function Form() {
     const [user, setUser] = useState([]);
@@ -8,7 +9,6 @@ function Form() {
         name: "",
         email: "",
         password: "",
-        reason: "",
         terms: ""
     });
 
@@ -43,30 +43,61 @@ function Form() {
 
     useEffect(() => {
         formSchema.isValid(formState).then(valid => {
-            console.log("valid?", valid)
+            console.log("valid: ", valid)
             setIsButtonDisabled(!valid)
         })
     }, [formState])
+
+    const formSubmit = e => {
+        e.preventDefault();
+        axios
+            .post("https://reqres.in/api/users", formState)
+            .then(response => {
+                console.log("subresp: ", response);
+                setUser(response.data);
+                setFormState({
+                    name: "",
+                    email: "",
+                    password: "",
+                    terms: ""
+                })
+            })
+            .catch(err => console.log("error: ", err.response));
+    };
+
+    const valueChange = e => {
+        console.log("input: ", e.target.value)
+        e.persist()
+        const newFormData = {
+            ...formState,
+            [e.target.name]:
+                e.target.type === "checkbox" ? e.target.checked : e.target.value
+        }
+        validateChange(e)
+        setFormState(newFormData)
+    };
     
     return (
-        <form>
+        <form onSubmit={formSubmit}>
             <label htmlFor="name">
                 Name:
-                <input id="name" type="name" name="name" placeholder="name" />
+                <input id="name" type="name" name="name" placeholder="name" onChange={valueChange} value={formState.name}/>
             </label>
             <label htmlFor="email">
-                Name:
-                <input id="email" type="email" name="email" placeholder="email" />
+                Email:
+                <input id="email" type="email" name="email" placeholder="email" onChange={valueChange} value={formState.email}/>
             </label>
             <label htmlFor="password">
                 Password
-                <input id="password" type="password" name="password" placeholder="Password" />
+                <input id="password" type="password" name="password" placeholder="Password" onChange={valueChange} value={formState.password} />
             </label>
             <label htmlFor="terms">
                 Do you agree to the terms and conditions?
-                <input id="terms" type="radio" name="terms" />
+                <input id="terms" type="checkbox" name="terms" checked={formState.checked} onChange={valueChange} />
             </label>
             <button type="submit" disabled={isButtonDisabled}>Submit!</button>
+
+            <pre>{JSON.stringify(user, null, 2)}</pre>
         </form>
     )
 };
