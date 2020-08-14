90dafb036e49fd4da0c955f92658e1223680daaf
JOliver23 <59593045+JOliver23@users.noreply.github.com>
2020-08-13 17:42:15 -0700

    Cypress.io MVP; test for missing entry failed to submit as expected; for test and submition test passing:

--- app-name/cypress/integration/form_test.js
+++ app-name/cypress/integration/form_test.js
@@ -1,11 +1,53 @@
+describe("test the form inputs w/ missing entries", function() {
+    this.beforeEach(function () {
+        cy.visit("http://localhost:3000/")
+    });
+
+    it("fills form entry w/ data", function(){
+
+        cy.get('[for="email"] > input')
+        .type("Mr_Le'Blob@bob.com")
+        .should("have.value", "Mr_Le'Blob@bob.com")
+
+        cy.get('#password')
+        .type("Bob-Le'Password")
+        .should("have.value", "Bob-Le'Password")
+
+        cy.get('#terms')
+        .check()
+        .should("be.checked")
+
+        cy.get('button')
+        // .click()
+        .should("be.disabled")
+        
+    })
+});
+
 describe("test the form inputs and submission", function() {
     this.beforeEach(function () {
         cy.visit("http://localhost:3000/")
     });
 
-    it("fills name entry w/ name", function(){
+    it("fills form entry w/ data", function(){
         cy.get('[data-cy = name')
         .type("Bob Le'Blob")
         .should("have.value", "Bob Le'Blob");
+
+        cy.get('[for="email"] > input')
+        .type("Mr_Le'Blob@bob.com")
+        .should("have.value", "Mr_Le'Blob@bob.com")
+
+        cy.get('#password')
+        .type("Bob-Password")
+        .should("have.value", "Bob-Password")
+
+        cy.get('#terms')
+        .check()
+        .should("be.checked")
+
+        cy.get('form')
+        .submit()
+        
     })
 });
